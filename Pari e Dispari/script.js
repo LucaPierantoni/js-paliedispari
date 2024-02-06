25479166261f8e720c4f9293011d17b96f0f08f1
LucaPierantoni <luca.pier99@gmail.com>
2024-02-06 20:21:19 +0100

    esercizio pari e dispari completo

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -5,3 +5,44 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Se l'utente ha indovinato l'esito (pari o dispari) della somma ha vinto, altrimenti ha perso.
 
+let randomNumber = 0;
+let numUtente = 0;
+
+function pariODispari(somma) {
+
+    const risultato = somma % 2 === 0;
+    return risultato;
+
+}
+
+function randomico(min, max) {
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+}
+
+function inserisciUnNumero() {
+
+    numUtente = Number(prompt("Inserisci un numero da 1 a 5: "));
+
+}
+
+const prosegui = document.getElementById('prosegui');
+const pariElement = document.getElementById('pari');
+const dispariElement = document.getElementById('dispari');
+
+prosegui.addEventListener('click', function() {
+    
+    inserisciUnNumero();
+    const NumeroComputer = randomico(1, 5);
+    document.body.innerHTML += `<p>il Computer ha "buttato" il numero: ${NumeroComputer}</p>`;
+    
+    const somma = numUtente + NumeroComputer;
+    if (pariODispari(somma) && pariElement.checked) {
+        document.body.innerHTML += "<p>Hai vinto!</p>";
+    } else if (!pariODispari(somma) && dispariElement.checked) {
+        document.body.innerHTML += "<p>Hai vinto!</p>";
+    } else {
+        document.body.innerHTML += "<p>Hai perso!</p>";
+    }
+});
